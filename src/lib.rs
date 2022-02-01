27a95fdcc7d85006dcf8e85b77842f523fbde2d1
Isaac Mills <rooster0055@protonmail.com>
2022-02-01 18:12:45 -0500

    So many faces

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+#![cfg_attr(all(feature = "bench", test), feature(test))]
+
 use std::fs::File;
-use std::io::{stdout, BufWriter, Error, ErrorKind, Write};
+use std::io::{BufWriter, Error, ErrorKind, Write};
 use std::path::Path;
 use std::str::from_utf8_unchecked;
 
-use indicatif::{ProgressBar, ProgressStyle};
+use ahash::RandomState;
 use linkify::{LinkFinder, LinkKind};
 use memmap::Mmap;
 
@@ -11,23 +13,41 @@ use constants::ACTIONS;
 use constants::ACTIONS_SIZE;
 use constants::FACES;
 use constants::FACES_SIZE;
-use seeder::UwUSeeder;
 
 mod constants;
-mod seeder;
 
 macro_rules! progress_bar {
-    ($bytes:expr) => {{
-        let progress_bar = ProgressBar::new($bytes);
-        progress_bar.set_style(
-            ProgressStyle::default_spinner()
-                .template("{spinner:.magenta} [{elapsed_precise:.bold}] {msg:.green.bold}"),
-        );
-        progress_bar.set_message("UwU'ifying In Progress...");
+    () => {{
+        let progress_bar = indicatif::ProgressBar::new_spinner()
+            .with_style(
+                indicatif::ProgressStyle::default_spinner()
+                    .template("{spinner:.magenta} [{elapsed_precise:.bold}] {msg:.green.bold}"),
+            )
+            .with_message("UwU'ifying In Progress...");
         progress_bar.enable_steady_tick(30);
 
         progress_bar
     }};
+}
+
+macro_rules! new_seeder {
+    ($word:expr, $random:expr) => {
+        <rand_xoshiro::Xoshiro256Plus as rand::SeedableRng>::seed_from_u64(
+            <[u8] as ahash::CallHasher>::get_hash($word, $random),
+        )
+    };
+}
+
+macro_rules! random_float {
+    ($seeder:expr) => {
+        rand::Rng::gen_range($seeder, 0.0..1.0)
+    };
+}
+
+macro_rules! random_int {
+    ($seeder:expr, $range:expr) => {
+        rand::Rng::gen_range($seeder, $range)
+    };
 }
 
 #[derive(Debug)]
@@ -39,7 +59,7 @@ pub struct UwUify<'a> {
     faces: f64,
     actions: f64,
     stutters: f64,
-    random: bool,
+    random: RandomState,
     is_runtime: bool,
     linkify: LinkFinder,
 }
@@ -54,7 +74,7 @@ impl<'a> Default for UwUify<'a> {
             faces: 0.05,
             actions: 0.125,
             stutters: 0.225,
-            random: false,
+            random: RandomState::with_seeds(69, 420, 28, 95),
             is_runtime: false,
             linkify: LinkFinder::new(),
         }
@@ -81,11 +101,14 @@ impl<'a> UwUify<'a> {
             text: text.unwrap_or_default(),
             input: infile.unwrap_or_default(),
             output: outfile.unwrap_or_default(),
-            random,
             is_runtime,
             linkify,
             ..Default::default()
         };
+
+        if random {
+            uwuify.random = RandomState::new();
+        }
 
         if let Some(words) = words {
             uwuify.words = words.parse::<f64>().unwrap();
@@ -103,7 +126,7 @@ impl<'a> UwUify<'a> {
         uwuify
     }
 
-    pub fn uwuify(&mut self) -> Result<(), Error> {
+    pub fn uwuify(&self) -> Result<(), Error> {
         // Handle Text
         if !self.text.is_empty() {
             // Handle Text Output
@@ -115,11 +138,14 @@ impl<'a> UwUify<'a> {
                     ));
                 }
 
-                let uwu_progress_bar = progress_bar!(self.text.len() as u64);
+                let uwu_progress_bar = progress_bar!();
                 self.uwuify_sentence(self.text, &mut BufWriter::new(File::create(&self.output)?))?;
                 uwu_progress_bar.finish_with_message("UwU'ifying Complete!");
             } else {
-                self.uwuify_sentence(self.text, &mut BufWriter::new(stdout().lock()))?;
+                #[cfg(not(test))]
+                self.uwuify_sentence(self.text, &mut BufWriter::new(std::io::stdout().lock()))?;
+                #[cfg(test)]
+                self.uwuify_sentence(self.text, &mut std::io::sink())?;
             }
         } else {
             // Handle File I/O
@@ -130,10 +156,9 @@ impl<'a> UwUify<'a> {
                 ));
             }
 
-            let infile = File::open(&self.input)?;
-            let uwu_progress_bar = progress_bar!(infile.metadata()?.len());
+            let uwu_progress_bar = progress_bar!();
             self.uwuify_sentence(
-                unsafe { from_utf8_unchecked(Mmap::map(&infile)?.as_ref()) },
+                unsafe { from_utf8_unchecked(Mmap::map(&File::open(&self.input)?)?.as_ref()) },
                 &mut BufWriter::new(File::create(&self.output)?),
             )?;
             uwu_progress_bar.finish_with_message("UwU'ifying Complete!");
@@ -142,19 +167,20 @@ impl<'a> UwUify<'a> {
         Ok(())
     }
 
-    pub fn uwuify_sentence<T: Write>(&mut self, text: &str, out: &mut T) -> Result<(), Error> {
+    pub fn uwuify_sentence<T: Write>(&self, text: &str, out: &mut T) -> Result<(), Error> {
         text.lines().try_for_each(|line| {
             line.split_whitespace()
                 .map(|word_str| word_str.as_bytes())
                 .try_for_each(|word| {
-                    let mut seeder = UwUSeeder::new(word, self.random);
-                    let random_value = seeder.random_float();
+                    let mut seeder = new_seeder!(word, &self.random);
+                    let random_value = random_float!(&mut seeder);
 
                     if !self.is_runtime {
                         if random_value <= self.faces {
-                            out.write_all(FACES[seeder.random_int(0..FACES_SIZE)])?;
+                            out.write_all(FACES[random_int!(&mut seeder, 0..FACES_SIZE)])?;
+                            out.write_all(b" ")?;
                         } else if random_value <= self.actions {
-                            out.write_all(ACTIONS[seeder.random_int(0..ACTIONS_SIZE)])?;
+                            out.write_all(ACTIONS[random_int!(&mut seeder, 0..ACTIONS_SIZE)])?;
                         } else if random_value <= self.stutters {
                             match word[0] {
                                 b'L' | b'R' => out.write_all(b"W"),
@@ -165,10 +191,10 @@ impl<'a> UwUify<'a> {
                         }
                     } else {
                         if random_value <= self.faces {
-                            out.write_all(FACES[seeder.random_int(0..FACES_SIZE)])?;
+                            out.write_all(FACES[random_int!(&mut seeder, 0..FACES_SIZE)])?;
                         }
                         if random_value <= self.actions {
-                            out.write_all(ACTIONS[seeder.random_int(0..ACTIONS_SIZE)])?;
+                            out.write_all(ACTIONS[random_int!(&mut seeder, 0..ACTIONS_SIZE)])?;
                         }
                         if random_value <= self.stutters {
                             match word[0] {
@@ -205,5 +231,27 @@ impl<'a> UwUify<'a> {
                 })?;
             out.write_all(b"\n")
         })
+    }
+}
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "bench")]
+    extern crate test;
+
+    #[cfg(feature = "bench")]
+    #[bench]
+    fn uwu_bench(b: &mut test::Bencher) {
+        let uwuify = super::UwUify::new(
+            Some(include_str!("test.txt")),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            false,
+            false,
+        );
+        b.iter(|| uwuify.uwuify());
     }
 }
